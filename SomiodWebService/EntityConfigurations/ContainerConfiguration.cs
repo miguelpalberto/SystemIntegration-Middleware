b24329c3cc83c4207c71c73045f59d61e6095a6e
João Silva <2211429@my.ipleiria.pt>
2023-12-30 16:48:22 +0000

    Refactor EntityConfigurations

--- SomiodWebService/EntityConfigurations/ContainerConfiguration.cs
+++ SomiodWebService/EntityConfigurations/ContainerConfiguration.cs
@@ -3,16 +3,17 @@ using System.Data.Entity.ModelConfiguration;
 
 namespace SomiodWebService.EntityConfigurations
 {
-	public class ApplicationConfiguration : EntityTypeConfiguration<Application>
+	public class ContainerConfiguration : EntityTypeConfiguration<Container>
 	{
-		public ApplicationConfiguration()
+		public ContainerConfiguration()
 		{
-			_ = ToTable("applications", "somiod");
+			_ = ToTable("containers", "somiod");
 			_ = HasKey(a => a.Id);
 			_ = Property(a => a.Id).HasColumnName("id");
 			_ = Property(a => a.Name).HasColumnName("name").HasMaxLength(255).IsRequired();
 			_ = HasIndex(a => a.Name).IsUnique();
 			_ = Property(a => a.CreatedDate).HasColumnName("creation_dt").IsRequired();
+			_ = Property(a => a.Parent).HasColumnName("parent").IsRequired();
 		}
 	}
 }
