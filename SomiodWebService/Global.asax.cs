82ad20cc6a537780878e2c4bf13d15d21d8c0b92
João Silva <2211429@my.ipleiria.pt>
2023-12-30 22:32:06 +0000

    fix

--- SomiodWebService/Global.asax.cs
+++ SomiodWebService/Global.asax.cs
@@ -3,18 +3,19 @@ using System.Web.Http;
 
 namespace SomiodWebService
 {
-    public class WebApiApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+	public class WebApiApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			GlobalConfiguration.Configure(WebApiConfig.Register);
 
-            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SomiodDbContext>());
+			Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SomiodDbContext>());
+			Database.SetInitializer(new MigrateDatabaseToLatestVersion<SomiodDbContext, Migrations.Configuration>());
 
-            using (SomiodDbContext context = new SomiodDbContext())
-            {
-                context.Database.Initialize(false);
-            }
-        }
-    }
+			using (var context = new SomiodDbContext())
+			{
+				context.Database.Initialize(false);
+			}
+		}
+	}
 }
